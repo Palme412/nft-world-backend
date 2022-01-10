a7932cb2da60e40656eb9c2e585497ddb6ed5c8a
Palme412 <jakepalmer412@gmail.com>
2022-01-10 11:32:14 -0600

    latest

--- collectionApi.js
+++ collectionApi.js
@@ -1,16 +1,16 @@
 const axios = require('axios');
 
-axios.get('https://api.opensea.io/api/v1/collection/doodles-official')
+axios.get('https://api.opensea.io/api/v1/collections?offset=0&limit=10')
     .then(response => {
-        // console.log("Data starts here", response.data);
+        console.log("Data starts here", response.data);
         console.log("Each,",
-            response.data.collection.name,
-            response.data.collection.stats.floor_price,
-            response.data.collection.stats.total_supply,
-            response.data.collection.stats.seven_day_sales,
-            response.data.collection.stats.thirty_day_sales,
-            response.data.collection.slug,
-            response.data.collection.description
+            response.data.collections.name,
+            response.data.collections.stats.floor_price,
+            response.data.collections.stats.total_supply,
+            response.data.collections.stats.seven_day_sales,
+            response.data.collections.stats.thirty_day_sales,
+            response.data.collections.slug,
+            response.data.collections.description
         );
     }).catch(error => {
         console.log("error", error);
